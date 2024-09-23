83e9e1a8571a71181d751b4ef1daf3fc16fb4c60
Noa Quenon <noa.quenon@proton.me>
2024-09-23 19:46:44 +0200

    extension: Add basic autocomplete

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,40 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const sections = [
+    "%info",
+    "%variables",
+    "%script",
+    "%config"
+]
+
 export function activate(context: vscode.ExtensionContext) {
+    console.log("Hello World!");
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "npf-language-support" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    const disposable = vscode.commands.registerCommand('npf-language-support.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from NPF Language Support!');
-    });
-
-    context.subscriptions.push(disposable);
+    const sectionCompletionProvider = vscode.languages.registerCompletionItemProvider(
+        { language: 'npf', scheme: 'file' },
+        {
+            provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+                const linePrefix = document.lineAt(position).text.substring(0, position.character);
+    
+                if (!linePrefix.startsWith('%')) return undefined;
+    
+                const typedSection = linePrefix.slice(1);
+    
+                const filteredSections = sections.filter(section =>
+                    section.startsWith(`%${typedSection}`)
+                );
+    
+                return filteredSections.map(section => {
+                    const item = new vscode.CompletionItem(section, vscode.CompletionItemKind.Keyword);
+                    item.insertText = section;
+                    return item;
+                });
+            }
+        },
+        '%'
+      );
+    
+      context.subscriptions.push(sectionCompletionProvider);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() { }
