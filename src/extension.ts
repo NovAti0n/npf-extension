c8d30174cbb4cd702cac6b23e5a318da246c71e6
Noa Quenon <noa.quenon@proton.me>
2024-09-24 10:05:40 +0200

    extension: Add sections autocompletion

--- src/extension.ts
+++ src/extension.ts
@@ -1,40 +1,29 @@
 import * as vscode from 'vscode';
-
-const sections = [
-    "%info",
-    "%variables",
-    "%script",
-    "%config"
-]
+import { sections } from './keywords';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log("Hello World!");
-
     const sectionCompletionProvider = vscode.languages.registerCompletionItemProvider(
         { language: 'npf', scheme: 'file' },
         {
             provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-                const linePrefix = document.lineAt(position).text.substring(0, position.character);
-    
-                if (!linePrefix.startsWith('%')) return undefined;
-    
-                const typedSection = linePrefix.slice(1);
-    
-                const filteredSections = sections.filter(section =>
-                    section.startsWith(`%${typedSection}`)
-                );
-    
-                return filteredSections.map(section => {
+                const linePrefix = document.lineAt(position).text.slice(0, position.character);
+
+                if (!linePrefix.endsWith('%')) return undefined;
+
+                const completionItems = sections.map(section => {
                     const item = new vscode.CompletionItem(section, vscode.CompletionItemKind.Keyword);
-                    item.insertText = section;
+                    item.insertText = new vscode.SnippetString(section);
+
                     return item;
                 });
+
+                return completionItems;
             }
         },
         '%'
-      );
+    );
     
-      context.subscriptions.push(sectionCompletionProvider);
+    context.subscriptions.push(sectionCompletionProvider);
 }
 
 export function deactivate() { }
---
+++ src/keywords.ts
@@ -0,0 +1,14 @@
+export const sections = [
+    "info",
+    "config",
+    "variables",
+    "late_variables",
+    "script",
+    "init",
+    "import",
+    "include",
+    "sendfile",
+    "require",
+    "pyexit",
+    "file"
+]
