125c4b1da42ab7720e077dcff776e69e0a578926
Noa Quenon <noa.quenon@proton.me>
2024-09-24 10:27:14 +0200

    extension: Add hover provider and documentation

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,21 @@ import * as vscode from 'vscode';
 import { sections } from './keywords';
 
 export function activate(context: vscode.ExtensionContext) {
+    // Provides autocompletion for section names
     const sectionCompletionProvider = vscode.languages.registerCompletionItemProvider(
         { language: 'npf', scheme: 'file' },
         {
             provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+                // Get the text before the cursor
                 const linePrefix = document.lineAt(position).text.slice(0, position.character);
 
                 if (!linePrefix.endsWith('%')) return undefined;
 
+                // Add a completion item for each section
                 const completionItems = sections.map(section => {
-                    const item = new vscode.CompletionItem(section, vscode.CompletionItemKind.Keyword);
-                    item.insertText = new vscode.SnippetString(section);
+                    const item = new vscode.CompletionItem(section.name, vscode.CompletionItemKind.Keyword);
+                    item.insertText = new vscode.SnippetString(section.name);
+                    item.documentation = new vscode.MarkdownString(section.description);
 
                     return item;
                 });
@@ -20,10 +24,27 @@ export function activate(context: vscode.ExtensionContext) {
                 return completionItems;
             }
         },
-        '%'
+        '%' // Trigger autocompletion when the user types '%'
     );
-    
-    context.subscriptions.push(sectionCompletionProvider);
+
+    const sectionHoverProvider = vscode.languages.registerHoverProvider(
+        { "language": "npf", "scheme": "file" },
+        {
+            provideHover(document, position) {
+                const range = document.getWordRangeAtPosition(position);
+                if (!range) return;
+
+                const word = document.getText(range);
+                const section = sections.find(s => s.name === word);
+                if (!section) return;
+
+                return new vscode.Hover(section.description, range);
+            }
+        }
+    );
+
+    // Register the providers
+    context.subscriptions.push(sectionCompletionProvider, sectionHoverProvider);
 }
 
 export function deactivate() { }
--- src/keywords.ts
+++ src/keywords.ts
@@ -1,14 +1,50 @@
 export const sections = [
-    "info",
-    "config",
-    "variables",
-    "late_variables",
-    "script",
-    "init",
-    "import",
-    "include",
-    "sendfile",
-    "require",
-    "pyexit",
-    "file"
+    {
+        "name": "info",
+        "description": "Contains information about what the test script does. The first line is used as graph title by default."
+    },
+    {
+        "name": "config",
+        "description": "Configuration options for the test execution and graph styling."
+    },
+    {
+        "name": "variables",
+        "description": "List of variables to define the matrix of parameters to test."
+    },
+    {
+        "name": "late_variables",
+        "description": "List of variables that are not to be considered part of the test, like constants."
+    },
+    {
+        "name": "script",
+        "description": "Bash commands to execute for the test. Can be defined to run with a specific role when appended with \`@rolename\`."
+    },
+    {
+        "name": "init",
+        "description": "Special script run once before all other scripts."
+    },
+    {
+        "name": "import",
+        "description": "Import another test script and optionally under a given role when appended with \`@rolename\`. NPF comes with *modules* test scripts intended for importation. They usually do specific tasks that can be re-used such as setting the CPU frequency."
+    },
+    {
+        "name": "include",
+        "description": "A test script to be included inline. It can be used to organise a test script in multiple files."
+    },
+    {
+        "name": "sendfile",
+        "description": "Send a file that lies with the test script to the remote machine. The source path of the file is given as first argument. The destination path is the test execution directory."
+    },
+    {
+        "name": "require",
+        "description": "Determines whether the test can run. NPF runs a test when all lines of the section returns a zero return code."
+    },
+    {
+        "name": "pyexit",
+        "description": "A Python script executed after the test. It can change and interpret its results."
+    },
+    {
+        "name": "file",
+        "description": "Creates a file with the filename given as first argument. The content of the file is the section content."
+    }
 ]
